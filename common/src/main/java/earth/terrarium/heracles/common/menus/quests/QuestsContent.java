7b1fcf0013e233bd62c5e1257bcca2e348fa1bd1
Felipe <40951999+fex0r@users.noreply.github.com>
2024-06-21 19:13:34 -0300

    feat: mapping questId to tasksLeft data, new widget description format

--- common/src/main/java/earth/terrarium/heracles/common/menus/quests/QuestsContent.java
+++ common/src/main/java/earth/terrarium/heracles/common/menus/quests/QuestsContent.java
@@ -8,27 +8,43 @@ import java.util.Map;
 
 public record QuestsContent(
     String group,
-    Map<String, ModUtils.QuestStatus> quests,
+    Map<String, ModUtils.QuestStatus> questId_to_questStatus,
+    Map<String, Long> questId_to_tasksLeft,
     boolean canEdit
 ) {
 
     public static QuestsContent from(FriendlyByteBuf buffer) {
         String group = buffer.readUtf();
-        Map<String, ModUtils.QuestStatus> quests = new HashMap<>();
-        int size = buffer.readVarInt();
-        for (int i = 0; i < size; i++) {
-            quests.put(buffer.readUtf(), buffer.readEnum(ModUtils.QuestStatus.class));
+        
+        Map<String, ModUtils.QuestStatus> questId_to_questStatus = new HashMap<>();
+        int questId_to_questStatus_size = buffer.readVarInt();
+        for (int i = 0; i < questId_to_questStatus_size; i++) {
+        	questId_to_questStatus.put(buffer.readUtf(), buffer.readEnum(ModUtils.QuestStatus.class));
         }
-        return new QuestsContent(group, quests, buffer.readBoolean());
+        
+        Map<String, Long> questId_to_tasksLeft = new HashMap<>();
+        int questId_to_tasksLeft_size = buffer.readVarInt();
+        for (int i = 0; i < questId_to_tasksLeft_size; i++) {
+        	questId_to_tasksLeft.put(buffer.readUtf(), buffer.readLong());
+        }
+        
+        return new QuestsContent(group, questId_to_questStatus, questId_to_tasksLeft, buffer.readBoolean());
     }
 
     public void to(FriendlyByteBuf buffer) {
         buffer.writeUtf(this.group);
-        buffer.writeVarInt(this.quests.size());
-        for (var entry : this.quests.entrySet()) {
+        buffer.writeVarInt(this.questId_to_questStatus.size());
+        for (var entry : this.questId_to_questStatus.entrySet()) {
             buffer.writeUtf(entry.getKey());
             buffer.writeEnum(entry.getValue());
         }
+        
+        buffer.writeVarInt(this.questId_to_tasksLeft.size());
+        for (var entry : this.questId_to_tasksLeft.entrySet()) {
+            buffer.writeUtf(entry.getKey());
+            buffer.writeLong(entry.getValue());
+        }
+        
         buffer.writeBoolean(this.canEdit());
     }
 }
